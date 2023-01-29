2b350dc1329c3bffc9bb7c1ad4a2978632dc777e
Muhammedhashirm009 <87589712+Muhammedhashirm009@users.noreply.github.com>
2023-01-29 14:55:56 +0530

    By hashir

--- send_message.sh
+++ send_message.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Twilio account details
-account_sid="AC2ea2995e3f8b38b2da9a65ebb8e6cb1b"
-auth_token="e1610ccefb0041240bfffd439c77ad11"
-messaging_service_sid="MGadcda36b858752c2b96eb45b3a3133e2"
+account_sid="account sid"
+auth_token="twilio suth token"
+messaging_service_sid="(your message sid) "
 
 
 send_message() {
@@ -32,7 +32,7 @@ if [ $messages_sent -gt 3 ]; then
 # If more than 3 messages have been sent, send logs file
 echo -e "\033[32mYou have reached the limit of 3 messages, logs will be sent\033[0m"
 curl 'https://api.twilio.com/2010-04-01/Accounts/'$account_sid'/Messages.json' -X POST
---data-urlencode 'To=+919037072660'
+--data-urlencode 'To=your number '
 --data-urlencode 'MessagingServiceSid='$messaging_service_sid
 --data-urlencode 'Body=Logs'
 --data-urlencode 'MediaUrl=http://yourserver.com/logs.txt'
